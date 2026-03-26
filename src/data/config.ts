dfbc0c8e484f0cebdc985a41369275af11a9f709
Rostyslav Mukha <snowyfl1cker@gmail.com>
2026-03-26 17:06:34 +0200

    Add footer

---
+++ src/data/config.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  isOpenToWork: false, 
+  
+  workplace: "GlobalLogic",
+  
+  statusMessages: {
+    open: "Open to new opportunities",
+    busy: "Currently at",
+  },
+
+  socials: [
+    { name: 'GitHub', href: 'https://github.com/iamavasya', icon: 'simple-icons:github' },
+    { name: 'LinkedIn', href: 'https://www.linkedin.com/in/rostyslav-mukha-0a2850273/', icon: 'simple-icons:linkedin' },
+    { name: 'Telegram', href: 'https://t.me/IAMAVASYA', icon: 'simple-icons:telegram' },
+    { name: 'Instagram', href: 'https://www.instagram.com/rostsky/', icon: 'simple-icons:instagram' },
+  ]
+};
